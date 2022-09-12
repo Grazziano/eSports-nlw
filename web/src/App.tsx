023d7162fc5fa75d38afc4ed3fac6879c17a536e
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-12 19:11:07 -0300

    Instalando tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,11 @@
+import './styles/main.css';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="w-2">
+      <h1>Olá mundo</h1>
+    </div>
+  );
 }
 
 export default App;
