3b1ef0958b49b0061499ca8cf1aad2b04949eba3
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-12 21:41:12 -0300

    Finalizando página Home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View } from 'react-native';
+import { FlatList, Image, View } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { GameCard } from '../../components/GameCard';
@@ -15,7 +15,15 @@ export function Home() {
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
       />
-      <GameCard data={GAMES[0]} />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
     </View>
   );
 }
