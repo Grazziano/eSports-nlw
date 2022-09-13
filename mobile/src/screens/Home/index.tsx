6c21164e33d28e00a3d11a35307fc2539d455d50
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-12 21:16:48 -0300

    Criando componente Header

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Header } from '../../components/Header';
 
 import { styles } from './styles';
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Header
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+    </View>
+  );
 }
