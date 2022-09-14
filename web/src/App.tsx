99acbd92d2d3f0b5ebfc49ccf67027d9c6a8f86f
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-14 19:46:14 -0300

    Listando games do backend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,28 @@ import './styles/main.css';
 import logoImg from './assets/logo-nlw-esports.svg';
 import GameBanner from './components/GameBanner';
 import CreateAdBanner from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+
+interface Game {
+  id: string;
+  title: string;
+  banner: string;
+  _count: {
+    ads: number;
+  };
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -17,11 +37,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="game-1.png"
-          title="League of Legends"
-          adsCount={2}
-        />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.banner}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
       </div>
 
       <CreateAdBanner />
