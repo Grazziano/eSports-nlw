683b698ab02bb77a4f9824b67a636c0610f8bab6
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-15 19:46:41 -0300

    Criando checkbox com radix

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,5 +1,6 @@
-import { GameController } from 'phosphor-react';
+import { Check, GameController } from 'phosphor-react';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as Checkbox from '@radix-ui/react-checkbox';
 
 import Input from './Form/Input';
 
@@ -81,8 +82,12 @@ export default function CreateAdModal() {
             </div>
           </div>
 
-          <div className="mt-2 flex gap-2 text-sm">
-            <Input type="checkbox" />
+          <div className="mt-2 flex items-center gap-2 text-sm">
+            <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+              <Checkbox.Indicator>
+                <Check className="w4 h-4 text-emerald-400" />
+              </Checkbox.Indicator>
+            </Checkbox.Root>
             Costumo me conectar ao chat de voz
           </div>
 
